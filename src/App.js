f62848545e058eeee3cc1e9db49897db25c1f4ba
inin <4292in@naver.com>
2022-04-07 15:21:56 +0900

    #7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-    return (
-      <div>
-        
-      </div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
